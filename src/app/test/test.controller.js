65a5828dceb3f685f5183b6758aa26f7d378b764
Pawel Potoniec <kontakt@pawelpotoniec.pl>
2016-03-14 13:07:08 +0100

    load cars from service

--- src/app/test/test.controller.js
+++ src/app/test/test.controller.js
@@ -1,5 +1,21 @@
 export class TestController {
-  constructor() {
+  constructor(raceCarService) {
     'ngInject';
+
+    this.cars = [];
+    this.rc_service = raceCarService;
+
+    // load data
+    this.cars = this.loadCars(100);
+
+    console.log(this.cars);
+
+
+
+  }
+
+  loadCars(howMany)
+  {
+    return this.rc_service.getCars(howMany);
   }
 }
