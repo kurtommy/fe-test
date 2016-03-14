ea8db0403b232702002e808a65bdc309955c2bb5
Pawel Potoniec <kontakt@pawelpotoniec.pl>
2016-03-14 13:39:18 +0100

    angular task - complete

--- src/app/test/test.controller.js
+++ src/app/test/test.controller.js
@@ -5,8 +5,8 @@ export class TestController {
     this.cars = [];
     this.rc_service = raceCarService;
 
-    // load data
-    this.cars = this.loadCars(299);
+    // load random amount of cars from service - for pagination testing
+    this.cars = this.loadCars(Math.random()*100 + 100);
 
     //scope
     $scope.cars = this.cars;
@@ -14,6 +14,14 @@ export class TestController {
     $scope.page = 1;
     $scope.per_page = 10;
     $scope.pages = Math.ceil($scope.amount / $scope.per_page);
+
+    // paginate cars and pass to view filtered array
+    $scope.$watch("page + per_page", function () {
+      var offset = (($scope.page - 1) * $scope.per_page);
+      var end   = offset + $scope.per_page;
+
+      $scope.filteredCars = $scope.cars.slice(offset, end);
+    });
   }
 
   loadCars(howMany)
